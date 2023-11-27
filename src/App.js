8b9f86a7306e296fc8eec199d6c5e4d8064659c8
alex <alexandrtre2003@gmail.com>
2023-11-27 20:57:44 +0300

    add 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
 
 function App() {
+    const {tg, onToggleButton} = useTelegram();
+
     useEffect(() => {
         tg.ready();
     }, []);
-  return (
-    <div className="App">
-      work
-        <button onClick={onClose}>Закрыть</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <button onClick={onToggleButton}>toggle</button>
+        </div>
+    );
 }
 
 export default App;
